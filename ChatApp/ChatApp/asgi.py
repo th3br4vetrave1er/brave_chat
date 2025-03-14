5c5a342d0968380b08812b103a42402ecb7d093e
th3br4vetrave1er <tazdingodan@gmail.com>
2025-03-14 20:44:41 +0200

    travolta gif

---
+++ ChatApp/ChatApp/asgi.py
@@ -0,0 +1,25 @@
+"""
+ASGI config for ChatApp project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+from channels.routing import ProtocolTypeRouter
+from django.core.asgi import get_asgi_application
+
+
+django_asgi_app = get_asgi_application()
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ChatApp.settings')
+application = ProtocolTypeRouter(
+    {
+        'http': django_asgi_app,
+        # Just HTTP for now. (We can add other protocols later.)
+    }
+)
+
+ASGI_APPLICATION = 'ChatApp.asgi.application'
